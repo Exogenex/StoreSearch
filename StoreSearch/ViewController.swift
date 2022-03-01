0871f76becfaf07599fdffde5f327328cc19bec4
Micah Pennington <mpennin3@hotmail.com>
2022-03-01 14:53:23 -0500

    Initial Commit

---
+++ StoreSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by Micah on 3/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
